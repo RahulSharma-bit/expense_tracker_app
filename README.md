5a1b7b0a02dafb01a976ff56f3d4d6760bb57d98
RahulSharma019 <154053287+RahulSharma019@users.noreply.github.com>
2023-12-17 15:45:24 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Expense_tracker_App
+ I am Creating this App to manage Expenses So that people can track thier Expenses.
---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:flutter/material.dart';
+import 'package:expense_tracker/expenses.dart';
+
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 37, 184, 189),
+);
+
+var kDarkScheme = ColorScheme.fromSeed(
+  brightness: Brightness.dark,
+  seedColor: const Color.fromARGB(255, 5, 99, 125),
+);
+void main() {
+  runApp(
+    MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        useMaterial3: true,
+        colorScheme: kDarkScheme,
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkScheme.secondaryContainer,
+          margin: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkScheme.primaryContainer,
+            foregroundColor: kDarkScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontWeight: FontWeight.bold,
+                color: kColorScheme.onSecondaryContainer,
+                fontSize: 16,
+              ),
+            ),
+      ),
+      themeMode: ThemeMode.system,
+      home: const Expenses(),
+    ),
+  );
+}
